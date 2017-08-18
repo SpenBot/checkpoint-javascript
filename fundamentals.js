b561cf1e0bae42030135ceb126b63a31249f00ce
Spenser Holstein <spenser.holstein@gmail.com>
2017-08-18 10:05:03 -0400

    fundamentals completed

--- fundamentals.js
+++ fundamentals.js
@@ -39,3 +39,5 @@ var instructor = {
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+
+instructor.facial-hair = true;
